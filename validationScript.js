1e52d381aa8c121f0d7ba112c556f4f001f32048
extf8 <extfield8@gmail.com>
2023-09-26 06:17:22 +0200

    Refactored validation script

--- validationScript.js
+++ validationScript.js
@@ -1,59 +1,75 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Add event listeners and error messages
-    function validateField(element, validationFunction) {
-        element.addEventListener('input', function (event) {
-            const errorElement = document.querySelector(`#${element.id}-error`);
-            const errorMessage = validationFunction(
-                element.value,
-                element.validity
-            );
-            errorElement.textContent = errorMessage;
-        });
-    }
+// Add event listeners to input fields
+const inputFields = [
+    'first-name',
+    'last-name',
+    'email',
+    'phone-number',
+    'password',
+    'confirm-password',
+];
+
+inputFields.forEach((field) => {
+    const inputElement = document.getElementById(field);
+    inputElement.addEventListener('input', validateInput);
 });
-// Add Validation functions
-function validateTextPresence(value) {
-    return value === '' ? 'This field is required' : '';
+
+// Validation function
+
+function validateInput(event) {
+    const inputElement = event.target;
+    const inputValue = inputElement.value;
+    const inputId = inputElement.id;
+    const errorElement = document.getElementById(`${inputId}-error`);
+
+    let errorMessage = '';
+
+    // Validation based on input id
+    switch (inputId) {
+        case 'first-name':
+        case 'last-name':
+            if (inputValue.length < 2) {
+                errorMessage = 'Must be at least 2 characters';
+            }
+            break;
+
+        case 'email':
+            const emailPattern =
+                /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+            if (!emailPattern.test(inputValue)) {
+                errorMessage = 'Invalid email address';
+            }
+            break;
+
+        case 'phone-number':
+            if (inputValue.length !== 10) {
+                errorMessage = 'Phone number must have 10 digits';
+            }
+            break;
+
+        case 'password':
+            if (inputValue.length < 6) {
+                errorMessage = 'Password must be at least 6 characters long';
+            }
+            break;
+
+        case 'confirm-password':
+            const passwordValue = document.getElementById('password').value;
+            if (inputValue !== passwordValue) {
+                errorMessage = 'Passwords do not match';
+            }
+            break;
+
+        default:
+            break;
+    }
+
+    // Show error message
+    errorElement.innerText = errorMessage;
+
+    // Add or remove error class based on validation
+    if (errorMessage) {
+        inputElement.classList.add('error-input');
+    } else {
+        inputElement.classList.remove('error-input');
+    }
 }
-
-function validateEmail(value, validity) {
-    return validity.typeMismatch ? 'Please enter valid email' : '';
-}
-
-function validatePhoneNumber(value, validity) {
-    return validity.patternMismatch
-        ? 'Please enter a 10-digit phone number'
-        : '';
-}
-
-function validatePassword(value) {
-    let errors = [];
-
-    if (!/[A-Z]/.test(value)) errors.push('Missing at least 1 capital letter');
-    if (!/[0-9]/.test(value)) errors.push('Missing al least 1 number');
-    if (value.length < 9) errors.push('Must be at least 8 characters');
-
-    return errors.join(`\n`);
-}
-
-function validateConfirmPassword(value, _, refElement) {
-    return value !== refElement.value ? 'Passwords do not match' : '';
-}
-
-// Select the input fields and error div's
-const firstName = document.querySelector('#first-name');
-const lastName = document.querySelector('#last-name');
-const email = document.querySelector('#email');
-const phoneNumber = document.querySelector('#phone-number');
-const password = document.querySelector('#password');
-const confirmPassword = document.querySelector('#confirm-password');
-
-// Validation
-validateField(firstName, validateTextPresence);
-validateField(lastName, validateTextPresence);
-validateField(email, validateEmail);
-validateField(phoneNumber, validatePhoneNumber);
-validateField(password, validatePassword);
-validateField(confirmPassword, (value, validity) =>
-    validateConfirmPassword(value, validity, password)
-);
